97327d67ad7625bb586900e6f12450b288b695e4
andydlin <andydlin@gmail.com>
2016-05-16 15:40:11 -0700

    adsf

--- explore-it/js/explore-it.js
+++ explore-it/js/explore-it.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
     $.Finger.preventDefault = true;
-    
+
     $('.box1').on('drag', function(e) {
         if(e.end) {
-            $('.box2').animate({
-                'top': 0
+            $('.box2').css({
+                '-webkit-transition': 'transform 0.5s',
+                '-webkit-transform': 'rotate(0deg)',
+                'transform': 'rotate(0deg)'
             },500);
         } else {
             $('.box2').css({
-                'top': e.dy
+                '-webkit-transition': 'transform 0s',
+                '-webkit-transform': 'rotate(' + e.dy + 'deg)',
+                'transform': 'rotate(' + e.dy + 'deg)'
             });
         }
     });
