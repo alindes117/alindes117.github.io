e8fe0c1a46d4a2817eae92cc10d583e5a6065f1b
andydlin <andydlin@gmail.com>
2016-05-23 15:07:16 -0700

    changes

--- explore-it/js/explore-it.js
+++ explore-it/js/explore-it.js
@@ -1,19 +1,30 @@
 $(document).ready(function() {
-    $.Finger.preventDefault = true;
+    setTimeout(function() {
+        $('body').addClass('active');
+    }, 500);
 
-    $('.box1').on('drag', function(e) {
-        if(e.end) {
-            $('.box2').css({
-                '-webkit-transition': 'transform 0.5s',
-                '-webkit-transform': 'rotate(0deg)',
-                'transform': 'rotate(0deg)'
-            },500);
-        } else {
-            $('.box2').css({
-                '-webkit-transition': 'transform 0s',
-                '-webkit-transform': 'rotate(' + (e.dy/2) + 'deg)',
-                'transform': 'rotate(' + (e.dy/2) + 'deg)'
-            });
-        }
-    });
+    if($('body').hasClass('home')) {
+        setTimeout(function() {
+            $('.content .inner').attr('style', '');
+        }, 2000);
+    }
+
+
+//     $.Finger.preventDefault = true;
+
+//     $('.box1').on('drag', function(e) {
+//         if(e.end) {
+//             $('.box2').css({
+//                 '-webkit-transition': 'transform 0.5s',
+//                 '-webkit-transform': 'rotate(0deg)',
+//                 'transform': 'rotate(0deg)'
+//             },500);
+//         } else {
+//             $('.box2').css({
+//                 '-webkit-transition': 'transform 0s',
+//                 '-webkit-transform': 'rotate(' + (e.dy/2) + 'deg)',
+//                 'transform': 'rotate(' + (e.dy/2) + 'deg)'
+//             });
+//         }
+//     });
 });
